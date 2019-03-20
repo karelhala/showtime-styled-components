5e6ca1daaa0282456ad5dee73952e8d01c6bcc52
Karel Hala <khala@redhat.com>
2019-03-20 18:29:13 +0100

    Add basic components

---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Card from './Card';
+
+export default () => (
+    <main>
+        <Card>
+            This is Card
+        </Card>
+    </main>
+)
---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default () => (
+    <aside>
+        <ul>
+            <li>
+                First nav
+            </li>
+            <li>
+                Second nav
+            </li>
+            <li>
+                third nav
+            </li>
+        </ul>
+    </aside>
+)
